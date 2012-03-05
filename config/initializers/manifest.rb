aa4a9d0257ada6638c8d681b79da3d8bb65d9fd9
Jonathan Clem <j@jclem.net>
2012-03-05 03:32:50 -0500

    Manifest initializer to touch files with data_type_for_manifest

---
+++ config/initializers/manifest.rb
@@ -0,0 +1,23 @@
+module Manifest
+  def self.add_app_models_to_manifest_data_types
+    models = Dir.glob("#{Rails.root}/app/models/*")
+
+    models.each do |m|
+      klass = nil
+      acts_as_manifest = false
+
+      File.open(m, 'r') do |f|
+        if f.lines.to_a.select { |l| l =~ /data_type_for_manifest/ }.count > 0
+          f.rewind
+          f.lines.each do |l|
+            if l =~ /ActiveRecord::Base/
+              l.split('<')[0].split(' ')[1].strip.constantize
+            end
+          end
+        end
+      end
+    end
+  end
+end
+  
+Manifest.add_app_models_to_manifest_data_types
