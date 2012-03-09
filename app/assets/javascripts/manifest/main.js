4ad206ab0df0fdb0ecfc64cb8bac4edaee0d9449
Jonathan Clem <j@jclem.net>
2012-03-09 13:25:20 -0500

    . pjax for mobile navigation. . 'minor' table th/td class. . touchBind

--- app/assets/javascripts/manifest/main.js
+++ app/assets/javascripts/manifest/main.js
@@ -4,9 +4,26 @@
 //= require_tree .
 
 $(function() {
+  $.fn.touchBind = function(fn) {
+    $(this).live('click', function() {
+      return fn.call(this);
+    })
+
+    $(this).live('touchmove', function() {
+      $(this).addClass('dragged');
+    });
+
+    $(this).live('touchend', function() {
+      if (!$(this).hasClass('dragged')) fn.call(this);
+      $(this).removeClass('dragged');
+    });
+
+    return this;
+  }
+
   $('.tinymce').tinymce({});
 
-  $('#content_block_allow_html').change(function() {
+  $('#content_block_allow_html').live('change', function() {
     if ($(this).is(':checked')) {
       $('#content_block_content').tinymce({});
     } else {
@@ -17,12 +34,15 @@ $(function() {
   })
 
   $(".nav-select").change(function() {
-    window.location = $(this).find("option:selected").val();
+    $.pjax({
+      url: $(this).find('option:selected').val(),
+      container: '.app-content'
+    })
   });
 
   $('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])').pjax('.app-content', { timeout: 10000 });
 
-  $('tbody tr').live('click', function() {
+  $('tbody tr').touchBind(function() {
     controller = $(this).parent().parent().attr('id').split(':').pop();
     id = $(this).attr('id').split('_').pop();
 
